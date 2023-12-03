d187bbe19d27d5d1fafe79de39a113a6a483dcd5
kttsu <150462533+kttsu@users.noreply.github.com>
2023-12-04 03:30:30 +0900

    Add files via upload

---
+++ src/test/java/com/helloworld/great/GreatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloworld.great;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
