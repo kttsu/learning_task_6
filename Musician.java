78f467789e8e63f5f23a9c1224581e9b8c20b97c
kttsu <150462533+kttsu@users.noreply.github.com>
2023-12-05 22:23:17 +0900

    Megadethのメンバーを表記する

---
+++ Musician.java
@@ -0,0 +1,20 @@
+package com.helloworld.great;
+
+public class Musician {
+    private final String name;
+    private final String position;
+
+    public Musician(String name, String position) {
+        this.name = name;
+        this.position = position;
+    }
+        public String getName() {
+            return name;
+
+        }
+        public String getPosition() {
+            return position;
+
+        }
+    }
+
